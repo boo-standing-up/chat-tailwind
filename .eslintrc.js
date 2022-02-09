725746243f6651fe7c8ef28f789ca7bb2119330f
BooPC <standingup.boo@gmail.com>
2022-02-09 21:01:42 +0900

    🚑 "next/core-web-vitals"削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:tailwindcss/recommended",
-    "next/core-web-vitals",
-  ],
+  extends: ["plugin:react/recommended", "plugin:tailwindcss/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
