0f8a7fce1ceaf3ea74f776fad04c509f1d9da711
BooPC <standingup.boo@gmail.com>
2022-02-25 19:23:54 +0900

    🗑️ crypto使えなかった

--- src/gravatar.js
+++
@@ -1,8 +0,0 @@
-import crypto from "crypto";
-import { isCryptoKey } from "util/types";
-
-export const gravatarPath = () => {
-  const md5 = crypto.createHash("md5");
-  const digest = md5.update("standingup.boo@gmail.com", "binary").digest("hex");
-  return "https://www.gravatar.com/avatar/${digest}/?d=retoro";
-};
