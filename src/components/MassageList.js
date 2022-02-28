8fd48a4923b4e0109f8990d5fc966c79415742ab
BooPC <standingup.boo@gmail.com>
2022-02-28 21:42:18 +0900

    🗃️ FireBaseからデータ取得

--- src/components/MassageList.js
+++ src/components/MassageList.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  limitToFirst,
+  orderByKey,
+} from "firebase/database";
 
 const MassageList = () => {
+  const [massages, setMassages] = useState([]);
+  const db = getDatabase();
+  const starCountRef = ref(db, "/message");
+
+  useEffect(() => {
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      //  console.log(Object.entries(data)); 配列にしました。
+      const entries = Object.entries(data);
+      const newMessages = entries.map((entry) => {
+        //0: Array(2)
+        //0: "-Mwl3Nz8-jHsZ4oFAlG9"　←key
+        //1: {name: 'Boo', text: 'どうなる'}　←NameAndText
+        const [key, NameAndText] = entry;
+        return { key, ...NameAndText };
+      });
+      console.log(newMessages);
+      setMassages(newMessages);
+    });
+  }, []);
+
   return (
     <div className="flex flex-col h-full overflow-x-auto mb-4">
       <div className="flex flex-col h-full">
