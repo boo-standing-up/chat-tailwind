e2763bc1668c5d854a3fda251de81d8d9b706de2
BooPC <standingup.boo@gmail.com>
2022-03-01 18:46:59 +0900

    ✨  message一覧表示

--- src/components/MassageList.js
+++ src/components/MassageList.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { getDatabase, ref, onValue } from "firebase/database";
 
-const MassageList = () => {
-  const [massages, setMassages] = useState([]);
+const MassageList = (name) => {
+  const [messages, setMessages] = useState([]);
   const db = getDatabase();
   const starCountRef = ref(db, "/message");
 
@@ -19,25 +19,30 @@ const MassageList = () => {
         return { key, ...NameAndText };
       });
       console.log(newMessages);
-      setMassages(newMessages);
+      setMessages(newMessages);
     });
   }, []);
 
   return (
-    <div className="flex flex-col h-full overflow-x-auto mb-4">
-      <div className="flex flex-col h-full">
-        <div className="grid h-full gap-y-2">
-          <div className="col-start-1 col-end-8 p-3 rounded-lg">
-            <div className="flex flex-row items-center">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative ml-3 text-2xl bg-white py-2 px-4 shadow rounded-xl">
-                <div>Hey How are you today?</div>
+    <div className="flex flex-col h-full overflow-x-auto mb-2 ">
+      <div className="flex flex-col gap-2">
+        {messages.map(({ key, name, text }) => {
+          return (
+            <div key={key} className="col-start-1 rounded-lg">
+              <div className="flex flex-row items-center">
+                <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                  A
+                </div>
+                <div className="grid relative ml-3 bg-white py-2 px-4 shadow rounded-xl">
+                  <div>
+                    <div className=" text-lg font-semibold">{name}</div>
+                    <div>{text}</div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/main.js
+++ src/components/main.js
@@ -9,7 +9,7 @@ const Main = (name) => {
         <div className="flex flex-col flex-auto h-full p-6">
           <div className="flex flex-col rounded-2xl bg-gray-100 h-screen p-4 overflow-y-hidden">
             <div className="flex-auto overflow-clip">
-              <MassageList />
+              <MassageList name={name} />
             </div>
             <div>
               <MassageInputField name={name} />
